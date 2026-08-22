d807f79a440e8a1cf4ddf27360e4c49e5f984723
Abidzar Dzakwan Sahudi <abidzardzakwan36@gmail.com>
2026-08-23 01:59:05 +0700

    feat: deploy to github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
     },
 
     app: {
+        baseURL: 'Himatifa-Platform-FE',
         head: {
             title: 'HIMATIFA - Himpunan Mahasiswa Informatika',
             meta: [
